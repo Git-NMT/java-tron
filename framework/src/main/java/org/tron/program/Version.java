feb6aaee7c2671b6229ab6b7faf2a4a1e0969d58
forfreeday <forfreeday@163.com>
2023-07-01 12:43:02 +0800

    update a new version. version name:GreatVoyage-v4.7.1.1,version code:17688

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.7.1-11-ge5e347de7";
-  public static final String VERSION_CODE = "17686";
+  public static final String VERSION_NAME = "GreatVoyage-v4.7.1.1";
+  public static final String VERSION_CODE = "17688";
   private static final String VERSION = "4.7.1.1";
 
   public static String getVersion() {
